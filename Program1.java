ecbe11f6922b948ddcc96e950d75b1d2cada3f66
harsh256qw34 <garshrajan@gmail.com>
2025-03-20 14:20:25 +0530

    hioiiiiiiiiii

---
+++ Program1.java
@@ -0,0 +1,10 @@
+public class Program1{
+    public static void main(String[] args) {
+        int a=2;
+        int b=8;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a+","+b);
+    }
+}
